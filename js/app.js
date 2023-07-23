8e1bf2fbc57e2ed5ab8361b068a82b4b8b5f99f7
jeronimo_nuñez <jeronimonunez96@gmail.com>
2023-07-23 18:16:37 -0300

    Log in terminado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+// swal.fire({
+//     title: "Bienvenidos!",
+//     confirmButtonText: "Ingresar",
+// });
+
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 
@@ -8,4 +13,19 @@ sign_up_btn.addEventListener('click', () =>{
 });
 sign_in_btn.addEventListener('click', () =>{
     container.classList.remove("sign-up-mode");
-})
+});
+
+function entrar () {
+    const usu = document.getElementById('usuarioPrincipal').value;
+    const pass = document.getElementById('passPrincipal').value;
+
+    if (usu == 'asd' && pass == '123') {
+        window.location.href = 'inicio.html';
+    } else {
+        swal.fire({
+            title: "Datos incorrectos!",
+            confirmButtonText: "Reingresar",
+        });
+    };
+};
+
